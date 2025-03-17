1065b17fda93b34e16f74f581a966a3b45a3dc6e
Ryan Wang <i@ryanc.cc>
2025-03-17 10:39:16 +0800

    Support extracting manifest file info from assets (#2)
    Support extracting manifest file info from assets.
    
    Previously, we obtained information by fixing the paths of plugin.yaml and theme.yaml, but in some plugins, it may not be possible to obtain due to different directory structures.
    
    This PR supports parsing plugin.yaml or theme.yaml in the artifacts from `assetsPath`.
    
    Fixes #1 
    
    ```release-note
    None
    ```

--- src/utils/api-client.ts
+++ src/utils/api-client.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
 import * as githubCore from "@actions/core";
+import axios from "axios";
 
 const baseURL = githubCore.getInput("halo-backend-baseurl");
 const pat = githubCore.getInput("halo-pat");
